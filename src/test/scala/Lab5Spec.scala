f22a1807f1f0e77356e95001222b9bdd0236b74e
U-Invictus\Jessica Lynch <Jessica Lynch@Invictus.(none)>
2014-12-07 19:28:04 -0700

    revised Lab5Spec.scala with more unit tests

--- src/test/scala/Lab5Spec.scala
+++ src/test/scala/Lab5Spec.scala
@@ -11,6 +11,43 @@ class Lab5Spec extends FlatSpec {
      assertResult((true,gold1)) { dowith(true) }
      assertResult((42,gold1)) { dowith(42) }
   }
+  
+  "DoNeg" should "negate values" in {
+    val e = Unary(Neg, N(42))
+    val (mp:Mem, ep: Expr) = step(e)(Mem.empty)
+    assertResult(N(-42)) { ep }
+  }
 
-  // Probably want to write some tests for castOk, typeInfer, substitute, and step.
+  "DoSeq" should "produce second element in sequence" in {
+    val e = Binary(Seq, N(1), Binary(Plus, N(2), N(3)))
+    val (mp:Mem, ep: Expr) = step(e)(Mem.empty)
+    assertResult(Binary(Plus, N(2), N(3))) { ep }
+  }
+  
+  "DoGetField" should "access a field from an object in memory" in {
+    val setup = Obj(Map("a" -> N(42), "b" -> N(47)))
+    val (m:Mem, addr: A) = step(setup)(Mem.empty)
+    val e = GetField(addr, "b")
+    val (mp, ep: Expr) = step(e)(m)
+    assert(m == mp)
+    assertResult(N(47)) { ep }
+  }
+
+  "DoVar" should "declare a variable" in {
+    val e = Decl(MVar, "x", N(10), Var("x"))
+    val (mp: Mem, ep: Expr) = step(e)(Mem.empty)
+    assert(ep match {
+      case Unary(Deref, a@A(_)) =>
+        // Verify memory correctly references N(10)
+        mp.get(a).get == N(10)
+      case _ => false
+      })
+  }
+
+  "SearchCall1" should "step its function" in {
+    val e = Call(If(B(true),Function(None,Left(List(("n",TNumber))),None,Var("n")),Null),List(N(42)))
+    val (_, ep: Expr) = step(e)(Mem.empty)
+    assertResult(
+      Call(Function(None, Left(List(("n", TNumber))), None, Var("n")), List(N(42)))) { ep }    
+  }
 }
